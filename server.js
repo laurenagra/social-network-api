515ffe756582404600d7494f1e598ebcc8a6b3fd
Lauren A <laurenhaileyama@gmail.com>
2022-05-04 10:30:01 -0400

    introduce file structure and base code in serverjs file

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const db = require('./config/connection');
+
+const app = express();
+const PORT = process.env.port || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use(require('./routes'));
