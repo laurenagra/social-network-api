ddb9b4aa2e7567540002c727a3730bee630a0823
Lauren A <laurenhaileyama@gmail.com>
2022-05-09 18:15:31 -0400

    Update seed.js in utils- begin testing

---
+++ utils/seed.js
@@ -0,0 +1,35 @@
+const connection = require('../config/connection');
+const { thought, user } = require('../models');
+
+connection.on('error', (err) => err);
+
+connection.once('open', async () => {
+  console.log('connected');
+
+  // Drop existing thought
+  await thought.deleteMany({});
+
+  // Drop existing user
+  await user.deleteMany({});
+
+  // Create empty array to hold the users
+  const users = [];
+
+  // Add user to the collection and await the results
+  await user.collection.insertOne({
+    username: "banana",
+    email: "banana@email.com"
+  });
+
+  // Add thought to the collection and await the results
+  await thought.collection.insertOne({
+    thoughtText: "Here's a cool thought...",
+    username: "bananana",
+    // thoughtId: "5edff358a0fcb779aa7b118b"
+  });
+
+  // Log out the seed data to indicate what should appear in the database
+  console.table(users);
+  console.info('Seeding complete! 🌱');
+  process.exit(0);
+});
